d3bd89051d430e06b6e79d6507624d875e66480b
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-08-17 10:25:48 +0330

    create components folder & Layout page

---
+++ components/layout/Layout.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+const Layout = ({children}) => {
+  return (
+    <>
+    <header className="header">
+        <h2>Management CRM</h2>
+        <Link href='/add-customer'>Add Customer</Link>
+    </header>
+    <div className='main'>{children}</div>
+    <footer className='footer'>
+        <a href='https://github.com/jamalshamsi1988' target='_blank' rel='noreferrer'>Jamal Shamsi</a> 
+        Next.js | CRM Project &copy;
+    </footer>
+      
+    </>
+  );
+}
+
+export default Layout;
