4efdd56354e6bfeb5b6af6d9e26bdcc203a5d22a
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-09-04 11:25:28 +0330

    complete the Edit function

---
+++ components/template/CustomerEditPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CustomerEditPage = ({data, id}) => {
+    console.log(data , id)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CustomerEditPage
